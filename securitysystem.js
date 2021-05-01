fa2148a361a89f3f7e13b34ade15450e02fa3505
davecorporation11 <77331244+davecorporation11@users.noreply.github.com>
2021-05-01 19:44:50 +0200

    Create securitysystem.js

---
+++ securitysystem.js
@@ -0,0 +1,11 @@
+function security() {
+alert("Loading SECURITY SYSTEM...");
+var password = "dvco-12xCudj";
+var verify = prompt("Heslo:");
+  if (verify === "dvco-12xCudj") {
+    alert("developing page...");
+  }
+  else {
+    alert("error code");
+  }
+}

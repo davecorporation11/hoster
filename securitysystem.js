e8755cd1ce64f09ee36e7f43de5a1a47b93a1ae3
davecorporation11 <77331244+davecorporation11@users.noreply.github.com>
2021-05-01 20:18:47 +0200

    Update securitysystem.js

--- securitysystem.js
+++ securitysystem.js
@@ -1,4 +1,6 @@
 function security() {
+  alert("Váš požadavek zpracováváme.Pošleme vám e-mail s odkazem na vaši e-mailovou adresu.Naše e-mailová adresa je davecorporation@outlook.cz kde můžete poslat i zpětnou vazbu.!");
+        alert("V emailu bude i kód který zadáte do SECURITY SYSTEMu po odkliknutí OK pod touto zprávou");
 alert("Loading SECURITY SYSTEM...");
 var password = "dvco-12xCudj";
 var verify = prompt("Heslo:");

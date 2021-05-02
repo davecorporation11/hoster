65d017156d8b1d0e6f3b4c643d06c94879d2e6f0
davecorporation11 <77331244+davecorporation11@users.noreply.github.com>
2021-05-02 12:10:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var finish = newAudio();
+finish.src = "https://davecorporation11.github.io/hoster/finish.mp3";
 function sendMail(params) {
     var tempParams ={
         from_name:document.getElementById('name').value,
@@ -6,9 +8,8 @@ function sendMail(params) {
     };
     emailjs.send('service_74zks7g','template_dapxlql',tempParams)
     .then(function(res){
-        window.open("https://davecorporation11.github.io/hoster/finish.mp3")
+        finish.play();
         document.write("Váš požadavek zpracováváme.Pošleme vám e-mail s odkazem na vaši e-mailovou adresu.Naše e-mailová adresa je davecorporation@outlook.cz kde můžete poslat i zpětnou vazbu.!")
         security();
-        window.close("https://davecorporation11.github.io/hoster/finish.mp3")
     })
 }

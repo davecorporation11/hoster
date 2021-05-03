19aa23f3679e707fa9dd8283c831b13cc08f79a2
davecorporation11 <77331244+davecorporation11@users.noreply.github.com>
2021-05-03 14:27:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ function sendMail(params) {
     .then(function(res){
         var finish = newAudio();
         finish.src = "https://davecorporation11.github.io/hoster/finish.mp3";
-        finish.SetVolume(0.3);
         finish.play();
+        finish.SetVolume(0.3);
         document.write("Váš požadavek zpracováváme.Pošleme vám e-mail s odkazem na vaši e-mailovou adresu.Naše e-mailová adresa je davecorporation@outlook.cz kde můžete poslat i zpětnou vazbu.!")
         security();
+        
     })
 }

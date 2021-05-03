ed5fbe3d5272533b20d2370509b08d2808f01ef7
davecorporation11 <77331244+davecorporation11@users.noreply.github.com>
2021-05-03 13:35:01 +0200

    Create index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var finish = newAudio();
-finish.src = "https://davecorporation11.github.io/hoster/finish.mp3";
 function sendMail(params) {
     var tempParams ={
         from_name:document.getElementById('name').value,
@@ -8,6 +6,8 @@ function sendMail(params) {
     };
     emailjs.send('service_74zks7g','template_dapxlql',tempParams)
     .then(function(res){
+        var finish = newAudio();
+        finish.src = "finish.mp3";
         finish.play();
         document.write("Váš požadavek zpracováváme.Pošleme vám e-mail s odkazem na vaši e-mailovou adresu.Naše e-mailová adresa je davecorporation@outlook.cz kde můžete poslat i zpětnou vazbu.!")
         security();
